a89e6c802b7c41df272b3f422ef63072d6f38b2c
marc@roosoft.com <marc@roosoft.com>
2022-11-22 12:13:20 -0500

    created the block live page

---
+++ lib/bitcoin_explorer_web/live/block_live.ex
@@ -0,0 +1,18 @@
+defmodule BitcoinExplorerWeb.BlockLive do
+  use BitcoinExplorerWeb, :live_view
+
+  alias BitcoinExplorer.Block
+
+  @impl true
+  def mount(%{"height" => height_string}, _session, socket) do
+    {height, _remainder} = Integer.parse(height_string)
+    block = Block.get_by_height(height)
+
+    {
+      :ok,
+      socket
+      |> assign(:height, height)
+      |> assign(:block, block)
+    }
+  end
+end
--- lib/bitcoin_explorer_web/router.ex
+++ lib/bitcoin_explorer_web/router.ex
@@ -18,6 +18,7 @@ defmodule BitcoinExplorerWeb.Router do
     pipe_through :browser
 
     live "/", HomeLive, :index
+    live "/block/:height", BlockLive, :index
   end
 
   # Other scopes may use custom stacks.
