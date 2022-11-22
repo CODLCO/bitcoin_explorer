da0356c8001212fa076e1ad730d5e6c0db83600d
marc@roosoft.com <marc@roosoft.com>
2022-11-22 12:45:24 -0500

    process blocks until it fails

--- lib/bitcoin_explorer_web/live/home_live.ex
+++ lib/bitcoin_explorer_web/live/home_live.ex
@@ -1,18 +1,37 @@
 defmodule BitcoinExplorerWeb.HomeLive do
   use BitcoinExplorerWeb, :live_view
 
+  require Logger
+
   alias BitcoinExplorer.Block
+
+  @start 30000
+  @size 10000
 
   @impl true
   def mount(_params, _session, socket) do
-    height = 0
+    initial_height = @start
+
+    send(self(), {:get_block, initial_height})
+
+    {:ok,
+     socket
+     |> assign(:height, initial_height)
+     |> assign(:previous_block_hash, "")}
+  end
+
+  def handle_info({:get_block, height}, socket) when height > @start + @size do
+    {:noreply, socket}
+  end
+
+  def handle_info({:get_block, height}, socket) do
     block = Block.get_by_height(height)
 
-    {
-      :ok,
-      socket
-      |> assign(:height, height)
-      |> assign(:block, block)
-    }
+    send(self(), {:get_block, height + 1})
+
+    {:noreply,
+     socket
+     |> assign(:height, height)
+     |> assign(:previous_block_hash, inspect(block.header.previous_block_hash))}
   end
 end
