4b3693659f64d1b580a64b24d9bebf8049d03cdd
marc@roosoft.com <marc@roosoft.com>
2022-11-22 09:32:46 -0500

    removed the default HTML generated by the scaffolder

--- mix.exs
+++ mix.exs
@@ -48,7 +48,9 @@ defmodule BitcoinExplorer.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:bitcoin_core_client, "~> 0.1.0"},
+      {:bitcoinlib, "~> 0.4.1"}
     ]
   end
 
