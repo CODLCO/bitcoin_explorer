b6dabddfc31fa5431cf645e433f3c7dc8feab2c6
marc@roosoft.com <marc@roosoft.com>
2022-11-22 19:19:54 -0500

    display error messages in the web page

--- lib/bitcoin_explorer_web/live/block_live.ex
+++ lib/bitcoin_explorer_web/live/block_live.ex
@@ -6,13 +6,24 @@ defmodule BitcoinExplorerWeb.BlockLive do
   @impl true
   def mount(%{"height" => height_string}, _session, socket) do
     {height, _remainder} = Integer.parse(height_string)
-    {:ok, block} = Block.get_by_height(height)
 
-    {
-      :ok,
-      socket
-      |> assign(:height, height)
-      |> assign(:block, block)
-    }
+    case Block.get_by_height(height) do
+      {:ok, block} ->
+        {
+          :ok,
+          socket
+          |> assign(:height, height)
+          |> assign(:block, block)
+        }
+
+      {:error, message} ->
+        {
+          :ok,
+          socket
+          |> assign(:height, height)
+          |> assign(:block, nil)
+          |> assign(:message, message)
+        }
+    end
   end
 end
