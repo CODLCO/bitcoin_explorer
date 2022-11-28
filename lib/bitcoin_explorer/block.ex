caee443421cb59bbcb4567ffa13a100a57a50084
marc@roosoft.com <marc@roosoft.com>
2022-11-28 08:37:40 -0500

    added a function to return the first ever bitcoin transaction

--- lib/bitcoin_explorer/block.ex
+++ lib/bitcoin_explorer/block.ex
@@ -5,4 +5,24 @@ defmodule BitcoinExplorer.Block do
     BitcoinCoreClient.get_block_by_height(block_height)
     |> Block.decode()
   end
+
+  def get_first_transaction do
+    get_first_transaction(0)
+  end
+
+  defp get_first_transaction(block_height) do
+    {:ok, block} =
+      block_height
+      |> BitcoinCoreClient.get_block_by_height()
+      |> Block.decode()
+
+    case Enum.count(block.transactions) do
+      1 ->
+        get_first_transaction(block_height + 1)
+
+      _ ->
+        [_coinbase, first_transaction] ++ _ = block.transactions
+        first_transaction
+    end
+  end
 end
