a37d568b34ca532cb5b4c8acf61c9fcc697e92eb
marc@roosoft.com <marc@roosoft.com>
2022-11-28 08:27:59 -0500

    fixed the page controller test

--- test/bitcoin_explorer_web/controllers/page_controller_test.exs
+++ test/bitcoin_explorer_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule BitcoinExplorerWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Bitcoin Explorer"
   end
 end
