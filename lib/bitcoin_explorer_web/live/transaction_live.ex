40e8aa393f6af54121f893618fb219cb5f18eb9e
marc@roosoft.com <marc@roosoft.com>
2022-12-03 11:29:25 -0500

    fetch prevouts and display them

--- lib/bitcoin_explorer_web/live/transaction_live.ex
+++ lib/bitcoin_explorer_web/live/transaction_live.ex
@@ -12,12 +12,27 @@ defmodule BitcoinExplorerWeb.TransactionLive do
         :ok,
         socket
         |> assign(:transaction, transaction)
+        |> assign(:inputs, get_prevouts(transaction))
         |> assign(:total_output_sats, total_output_sats(transaction))
         |> assign(:format_sats, &format_sats/1)
       }
     else
       {:error, message} -> {:ok, socket |> assign(:error, message)}
     end
+  end
+
+  defp get_prevouts(%BitcoinLib.Transaction{inputs: inputs}) do
+    inputs
+    |> Enum.map(fn input ->
+      with {:ok, transaction} <- Transaction.get(input.txid) do
+        prevout =
+          transaction
+          |> Map.get(:outputs)
+          |> Enum.at(input.vout)
+
+        %{input: input, prevout: prevout}
+      end
+    end)
   end
 
   defp total_output_sats(%BitcoinLib.Transaction{outputs: outputs}) do
