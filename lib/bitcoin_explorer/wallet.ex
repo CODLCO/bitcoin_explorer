71a3855fe37f6b3baface01d9bf56407318a64a3
marc@roosoft.com <marc@roosoft.com>
2022-12-06 18:28:21 -0500

    moved wallet business logic to BitcoinExplorer namespace, out of live views

---
+++ lib/bitcoin_explorer/wallet.ex
@@ -0,0 +1,35 @@
+defmodule BitcoinExplorer.Wallet do
+  def get_utxos(xpub) do
+    entries = BitcoinAccounting.get_book_entries(xpub)
+
+    %{
+      receive: entries.receive |> add_sums,
+      change: entries.change |> add_sums
+    }
+  end
+
+  defp add_sums(thing) do
+    thing
+    |> Enum.map(fn stuff ->
+      credits =
+        stuff.history
+        |> Enum.map(fn yo ->
+          yo.credits
+          |> Enum.sum()
+        end)
+        |> Enum.sum()
+
+      debits =
+        stuff.history
+        |> Enum.map(fn yo ->
+          yo.debits
+          |> Enum.sum()
+        end)
+        |> Enum.sum()
+
+      stuff
+      |> Map.put(:sum_credits, credits)
+      |> Map.put(:sum_debits, debits)
+    end)
+  end
+end
--- lib/bitcoin_explorer_web/live/wallet_live.ex
+++ lib/bitcoin_explorer_web/live/wallet_live.ex
@@ -3,6 +3,8 @@ defmodule BitcoinExplorerWeb.WalletLive do
 
   require Logger
 
+  alias BitcoinExplorer.Wallet
+
   @impl true
   def mount(%{}, _session, socket) do
     [mnemonic_phrase: _, tpub: tpub] = Application.get_env(:bitcoin_explorer, :bitcoin)
@@ -10,42 +12,8 @@ defmodule BitcoinExplorerWeb.WalletLive do
     {
       :ok,
       socket
-      |> assign(:book_entries, get_entries(tpub))
+      |> assign(:book_entries, Wallet.get_utxos(tpub))
       |> assign(:hero, "The wallet")
     }
-  end
-
-  defp get_entries(tpub) do
-    entries = BitcoinAccounting.get_book_entries(tpub)
-
-    %{
-      receive: entries.receive |> add_sums,
-      change: entries.change |> add_sums
-    }
-  end
-
-  defp add_sums(thing) do
-    thing
-    |> Enum.map(fn stuff ->
-      credits =
-        stuff.history
-        |> Enum.map(fn yo ->
-          yo.credits
-          |> Enum.sum()
-        end)
-        |> Enum.sum()
-
-      debits =
-        stuff.history
-        |> Enum.map(fn yo ->
-          yo.debits
-          |> Enum.sum()
-        end)
-        |> Enum.sum()
-
-      stuff
-      |> Map.put(:sum_credits, credits)
-      |> Map.put(:sum_debits, debits)
-    end)
   end
 end
