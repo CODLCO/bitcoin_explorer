1522e48cbfb8c82754ad446442b4d8ebcbbd9bde
marc@roosoft.com <marc@roosoft.com>
2022-12-11 10:12:09 -0500

    added a link to mempool.space in a transaction page

--- lib/bitcoin_explorer_web/live/transaction_live/show.ex
+++ lib/bitcoin_explorer_web/live/transaction_live/show.ex
@@ -4,6 +4,7 @@ defmodule BitcoinExplorerWeb.TransactionLive.Show do
   require Logger
 
   alias BitcoinExplorer.Transaction
+  alias BitcoinExplorer.Environment
 
   @impl true
   def mount(%{"id" => id}, _session, socket) do
@@ -15,6 +16,7 @@ defmodule BitcoinExplorerWeb.TransactionLive.Show do
         |> assign(:inputs, get_prevouts(transaction))
         |> assign(:total_output_sats, total_output_sats(transaction))
         |> assign(:format_sats, &format_sats/1)
+        |> assign(:mempool_space_url, get_mempool_space_url(transaction, Environment.network()))
         |> assign(:hero, "transaction #{id}")
       }
     else
@@ -47,5 +49,13 @@ defmodule BitcoinExplorerWeb.TransactionLive.Show do
   defp format_sats(value) do
     formatted = :erlang.float_to_binary(value / 100_000_000, decimals: 8)
     "#{formatted} BTC"
+  end
+
+  defp get_mempool_space_url(%BitcoinLib.Transaction{id: id}, :mainnet) do
+    "https://mempool.space/tx/#{id}"
+  end
+
+  defp get_mempool_space_url(%BitcoinLib.Transaction{id: id}, :testnet) do
+    "https://mempool.space/testnet/tx/#{id}"
   end
 end
