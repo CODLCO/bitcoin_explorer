df9d8e4acac006def5a37d9d0e8320ae6670b67a
marc@roosoft.com <marc@roosoft.com>
2022-12-13 08:46:37 -0500

    some utxo code cleanup and send one from a list of selected utxos

---
+++ lib/bitcoin_explorer/utxo.ex
@@ -0,0 +1,36 @@
+defmodule BitcoinExplorer.Utxo do
+  def from_xpub(xpub) do
+    xpub
+    |> BitcoinAccounting.get_utxos()
+    |> Enum.map(&extract_utxo/1)
+    |> Enum.concat()
+    |> Enum.sort(fn %{value: value1}, %{value: value2} -> value1 > value2 end)
+    |> add_time
+  end
+
+  defp extract_utxo(
+         {%BitcoinAccounting.XpubManager.AddressInfo{
+            address: address,
+            change?: change?,
+            index: index
+          }, utxo_list}
+       ) do
+    utxo_list
+    |> Enum.map(fn utxo ->
+      utxo
+      |> Map.put(:address, address)
+      |> Map.put(:change?, change?)
+      |> Map.put(:index, index)
+      |> Map.put(:selected, false)
+    end)
+  end
+
+  defp add_time(utxo_list) do
+    utxo_list
+    |> Enum.map(fn utxo ->
+      transaction = ElectrumClient.get_transaction(utxo.transaction_id)
+
+      Map.put(utxo, :time, transaction.time)
+    end)
+  end
+end
